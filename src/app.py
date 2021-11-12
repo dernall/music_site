f36c7bb81d0d7f9d0ba6a949bf4c1cc1c86b1f71
Oleg Nesterenkov <olegnest20@gmail.com>
2021-11-12 19:41:27 +0300

    Removed checker

--- src/app.py
+++ src/app.py
@@ -3,10 +3,6 @@ from repository.database import Database
 
 if __name__ == "__main__":
     db = Database()
-    if db.connection is None:
-        print("OFDOFKDOFK")
-    else:
-        print("NICE")
     app = Flask(__name__)
 
     @app.route("/")
